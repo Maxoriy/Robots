02d41eb4ee60166c9c41472b1af6165a9477a678
maxoriy <anohin257@gmail.com>
2023-06-14 21:47:04 +0500

    Returned the movement of the robot to what it originally was

--- src/main/java/models/RobotModel.java
+++ src/main/java/models/RobotModel.java
@@ -31,19 +31,19 @@ public class RobotModel extends Observable {
     public void moveRobot(double velocity, double angularVelocity, double duration) {
         velocity = applyLimits(velocity, 0, maxVelocity);
         angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
-        double newX = m_robotPositionX;
-        double newY = m_robotPositionY;
-        double newDirection = m_robotDirection;
-        if (angularVelocity == 0) {
-            newX = m_robotPositionX + velocity * duration * Math.cos(m_robotDirection);
-            newY = m_robotPositionY + velocity * duration * Math.sin(m_robotDirection);
-        } else {
-            newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
-        }
+        double newX = m_robotPositionX + velocity / angularVelocity *
+                (Math.sin(m_robotDirection + angularVelocity * duration) -
+                        Math.sin(m_robotDirection));
+        double newY = m_robotPositionY - velocity / angularVelocity *
+                (Math.cos(m_robotDirection + angularVelocity * duration) -
+                        Math.cos(m_robotDirection));
+
+        if (!Double.isFinite(newX)) newX = m_robotPositionX + velocity * duration * Math.cos(m_robotDirection);
+        if (!Double.isFinite(newY)) newY = m_robotPositionY + velocity * duration * Math.sin(m_robotDirection);
+
         m_robotPositionX = newX;
         m_robotPositionY = newY;
-        m_robotDirection = newDirection;
-
+        m_robotDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
         setChanged();
         notifyObservers(this);
     }
@@ -54,11 +54,9 @@ public class RobotModel extends Observable {
 
         double angleToTarget = angleTo(m_robotPositionX, m_robotPositionY, targetModel.getTargetX(), targetModel.getTargetY());
         double angularVelocity = 0;
-
-        if (angleToTarget > m_robotDirection + 0.01) angularVelocity = maxAngularVelocity;
-
-        if (angleToTarget < m_robotDirection - 0.01) angularVelocity = -maxAngularVelocity;
-
+        if (angleToTarget > m_robotDirection) angularVelocity = maxAngularVelocity;
+        if (angleToTarget < m_robotDirection) angularVelocity = -maxAngularVelocity;
+        if (Math.abs(angleToTarget - m_robotDirection) > Math.PI) angularVelocity = -angularVelocity;
         moveRobot(maxVelocity, angularVelocity, 10);
 
     }
